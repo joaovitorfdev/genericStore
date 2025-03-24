3fa0fc155099d6a8db3330b354d2dde0315f3bb0
João Victor <joaovitorf.dev@gmail.com>
2025-03-24 12:45:51 -0400

    style

--- src/app/(public)/register/page.tsx
+++ src/app/(public)/register/page.tsx
@@ -46,8 +46,8 @@ export default function RegisterPage() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8 bg-white p-8 rounded-xl shadow-lg">
+    <div className="min-h-screen  flex items-center justify-center bg-white py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8 bg-white p-8 rounded-xl shadow-lg border border-gray-200 rounded-lg">
         <div>
           <h2 className="text-center text-3xl font-bold text-gray-900">
             Criar Conta
