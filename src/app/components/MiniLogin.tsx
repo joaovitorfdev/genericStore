1529714e8cddc31e618adc33f85c880344e60c5c
João Victor <joaovitorf.dev@gmail.com>
2025-03-26 17:50:49 -0400

    mini login

---
+++ src/app/components/MiniLogin.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { login } from "../auth";
+import Link from "next/link";
+
+export default function MiniLogin(){
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loginFailed, setLoginFailed] = useState(false);
+    
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const success = await login(username, password);
+        setLoginFailed(!success);
+      };
+    
+      const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (loginFailed) {
+          setLoginFailed(false); 
+        }
+        if (e.target.name === "username") {
+          setUsername(e.target.value);
+        } else {
+          setPassword(e.target.value);
+        }
+      };
+    return(
+        
+        <div className="absolute top-full right-0 mt-2 w-64 bg-white border border-gray-200 rounded-2xl shadow-lg z-10 p-4">
+          
+            <form
+              className="grid gap-3 "
+              onSubmit={handleSubmit}
+            >
+              <input
+                onChange={handleInputChange}
+                name="username"
+                type="text"
+                placeholder="Email"
+                className={`w-full px-3 py-2 border rounded-md focus:outline-none focus:border-black ${loginFailed ? "border-red-500" : "border-gray-200"}`}
+              />
+              <input
+                onChange={handleInputChange}
+                name="password"
+                type="password"
+                placeholder="Password"
+                className={`w-full px-3 py-2 border rounded-md focus:outline-none focus:border-black ${loginFailed ? "border-red-500" : "border-gray-200"}`}
+              />
+              <button
+                type="submit"
+                className="w-full bg-gray-800 text-white px-3 py-2 rounded-md"
+              >
+                Entrar
+              </button>
+              
+            </form>
+            <div className="text-center text-sm mt-4">
+          <p className="text-gray-600">
+            Não tem uma conta?{" "}
+            <Link
+              href="/register"
+              className="font-medium text-gray-800 hover:text-gray-600 cursor-pointer transition-colors duration-200"
+            >
+              Registre-se
+            </Link>
+          </p>
+        </div>
+        {   
+            loginFailed && (
+            <div className="text-center mt-4">
+                <p className="text-red-600 text-sm">
+                Login ou senha invalidos
+                </p>
+            </div>
+        )}
+        </div>
+    )
+}
--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,16 +1,13 @@
-"use client";
-
+"use client"
+import { ShoppingCartIcon, UserCircleIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import MiniLogin from "./MiniLogin";
 import { usePathname } from "next/navigation";
-import { UserCircleIcon, ShoppingCartIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function NavBar() {
   const pathname = usePathname();
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [loginFailed, setLoginFailed] = useState(false);
   const toggleDropdown = () => setDropdownOpen((prev) => !prev);
 
   const navLinks = [
@@ -18,18 +15,27 @@ export default function NavBar() {
     { href: "/shop", label: "SHOP" },
     { href: "/about", label: "ABOUT" },
   ];
+ 
+  useEffect(() => {
+    if (pathname === "/register" || pathname === "/login") {
+      setDropdownOpen(false);
+    }
+  }, [pathname]);
 
   return (
     <nav className="bg-white medium-text w-full text-black p-4 flex items-center h-28 relative">
+      {/* Logo à esquerda */}
       <div className="flex justify-center w-full md:w-auto md:justify-start">
         <img
+          
           src="https://cdn.discordapp.com/attachments/1336112434666012716/1353925131587813437/pbmz_aim.png?ex=67e36c8a&is=67e21b0a&hm=9538c880ab9fd28ebd0e7df5b961d0cc05980386f3c718f88fab74e03dda3306&"
           alt="Logo"
           className="w-40"
         />
+         
       </div>
 
-      <div className="hidden md:flex flex-1 justify-center text-sm space-x-5 md:text-lg md:space-x-20 font-semibold">
+      <div className="hidden md:flex flex-1 justify-center space-x-10 text-sm md:text-lg font-semibold">
         {navLinks.map((link) => {
           const isActive = pathname === link.href;
           return (
@@ -55,35 +61,7 @@ export default function NavBar() {
           <span>Entrar</span>
         </div>
 
-        {
-          dropdownOpen && (
-          <div className="absolute top-full right-0 mt-2 w-64 bg-white border border-gray-200 rounded-md shadow-lg z-10 p-4">
-            <form
-              className="grid gap-3"
-              onSubmit={(e) => {
-                e.preventDefault();
-                
-              }}
-            >
-              <input
-                type="text"
-                placeholder="Email"
-                className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-black"
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-black"
-              />
-              <button
-                type="submit"
-                className="w-full bg-gray-800 text-white px-3 py-2 rounded-md"
-              >
-                Login
-              </button>
-            </form>
-          </div>
-        )}
+        { dropdownOpen && (<MiniLogin></MiniLogin>)  }
 
         <ShoppingCartIcon className="h-8 w-8 ml-10 md:h-10 md:w-10" />
       </div>
